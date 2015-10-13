18569c723b397a443bb435fce09117e2735a388e
yfaizal <yfaizal@myphonefax.com>
2015-10-13 16:52:48 +0800

    Update token_table.sql
    Update text info

--- sql/token_table.sql
+++ sql/token_table.sql
@@ -15,4 +15,4 @@ CREATE TABLE IF NOT EXISTS `onlinefaxes_token` (
 
 INSERT INTO `onlinefaxes_token` (`id`, `token`, `note`) VALUES
 (1, 'Lzo2MDY2OTI3MDc5MWI0NjBkYTFmN2FmZGVhNWQ5NTE4YzpyQjBlRTFZMW1ZNHZwbEpJRGFsWm0xczQ1MUV2VjhzN3VTWUM0ZDVZQ0JZPTpjYzAzM2E3ODAzYzM0M2Q5YTZhNmE3MWVhNWU2MjcyZToxNDQ0MDEwNjgw', 'Active token , will update when request a new one'),
-(2, 'Lzo2MDY2OTI3MDc5MWI0NjBkYTFmN2FmZGVhNWQ5NTE4YzpBUnplcXRUK1dSTW9vRGZBbUpMVjlHMVhPVUQyTTVyenZmMGREY1hTdEpjPTo5YTkyNjFkYjcxMjE0MjNkYjI3ODIwOGU2ZjMyOTdhNzoxNDQyMzkyNjE3', 'This is an expired token. Use for testing purpose');
+(2, 'Lzo2MDY2OTI3MDc5MWI0NjBkYTFmN2FmZGVhNWQ5NTE4YzpBUnplcXRUK1dSTW9vRGZBbUpMVjlHMVhPVUQyTTVyenZmMGREY1hTdEpjPTo5YTkyNjFkYjcxMjE0MjNkYjI3ODIwOGU2ZjMyOTdhNzoxNDQyMzkyNjE3', 'This is for an expired token. Use for testing expired token scenario');
